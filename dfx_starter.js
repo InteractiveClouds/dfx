15945483db76dc8f32ed786e0be1b74c9a70bf2e
opoupeney <olivier.poupeney@icloud.com>
2016-07-13 15:02:34 -0400

    Change DFX version number in all headers

--- dfx_starter.js
+++ dfx_starter.js
@@ -2,7 +2,7 @@
  This notice must be untouched at all times.
 
  DreamFace DFX
- Version: 2.0.0
+ Version: 3.0.0
  Author: Interactive Clouds
 
  Copyright (c) 2015 Interactive Clouds, Inc.  "DreamFace" is a trademark of Interactive Clouds, Inc.
@@ -21,19 +21,7 @@ var dreamface = require('./dfx')
 	app_build_path: path.resolve(__dirname, './app_builds'),
 	edition: 'development',
 	storage: 'mongod',
-	deployment_server_host: '192.168.12.15',
+	deployment_server_host: 'localhost',
 	deployment_server_port: 3030,
 })
 .start();
-
-
-/*
-,
-	docker_daemon : {
-        host : '192.168.99.100',
-        port : process.env.DOCKER_PORT || 2376,
-        ca   : fs.readFileSync('/Users/olivier/.docker/machine/certs/ca.pem').toString('utf8'),
-        cert : fs.readFileSync('/Users/olivier/.docker/machine/certs/cert.pem').toString('utf8'),
-        key  : fs.readFileSync('/Users/olivier/.docker/machine/certs/key.pem').toString('utf8')
-    }
-*/
