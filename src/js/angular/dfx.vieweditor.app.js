6e4c184f5ebc7bdfa76006093aead46c1b68cc15
vinnie777 <mtarassov@gmail.com>
2016-08-10 14:39:42 +0200

    tabs flex layout

--- src/js/angular/dfx.vieweditor.app.js
+++ src/js/angular/dfx.vieweditor.app.js
@@ -1156,7 +1156,9 @@ dfxViewEditorApp.controller("dfx_view_editor_controller", [ '$scope', '$rootScop
         var gc_instance = {};
         var flex_container_attr = (component.flex=='true' || (component.attributes!=null && component.attributes.flex!=null)) ? ' flex="{{attributes.flex.value}}"' : '';
 
-        var panel_layout = (component.type == 'panel' && component.attributes && (!component.attributes.autoHeight ||  component.attributes.autoHeight.value != true)) ? ' style="height:100%;" layout="column" ' : '';
+        var panel_layout = ((component.type == 'panel' || component.type == 'tabs') &&
+            component.attributes && (!component.attributes.autoHeight ||  component.attributes.autoHeight.value != true)) ?
+                ' style="height:100%;" layout="column" ' : '';
 
         gc_instance.fragment = $compile('<div id="' + component.id + '" dfx-gc-web-base dfx-gc-web-' + component.type + ' dfx-gc-design gc-type="' + component.type + '" gc-role="control"' + flex_container_attr + panel_layout + '></div>')($scope);
         gc_instance.id = component.id;
