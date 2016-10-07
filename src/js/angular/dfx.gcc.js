87718f94622ad9d36b0c6758556981051d123efa
Andriy <andriy292253@gmail.com>
2016-10-07 15:38:20 +0300

    fixed styles/classes

--- src/js/angular/dfx.gcc.js
+++ src/js/angular/dfx.gcc.js
@@ -498,17 +498,17 @@ dfxGCC.directive('dfxGccWebCarousel', ['$http', '$sce', '$mdDialog', '$mdToast',
                     }, 0);
                 }
                 scope.parseSlideSrc = function() {
-                    for ( var i = 0; i < scope.attributes.static.value.length; i++ ) {
-                        var testSrc = scope.attributes.static.value[i].src;
-                        if (testSrc.indexOf("'") == -1) {
-                            scope.attributes.static.value[i].parsedSrc = scope.$gcscope[scope.attributes.static.value[i].src];
-                        } else if (testSrc.indexOf("'") == 0 && testSrc.lastIndexOf("'") == (testSrc.length - 1) && testSrc.length > 2) {
-                            var srcWithoutQuotes = testSrc.replace(/'/g, '');
-                            scope.attributes.static.value[i].parsedSrc = srcWithoutQuotes;
-                        } else {
-                            scope.attributes.static.value[i].parsedSrc = scope.attributes.static.value[i].src;
-                        }
-                    }
+                    // for ( var i = 0; i < scope.attributes.static.value.length; i++ ) {
+                    //     var testSrc = scope.attributes.static.value[i].src;
+                    //     if (testSrc.indexOf("'") == -1) {
+                    //         scope.attributes.static.value[i].parsedSrc = scope.$gcscope[scope.attributes.static.value[i].src];
+                    //     } else if (testSrc.indexOf("'") == 0 && testSrc.lastIndexOf("'") == (testSrc.length - 1) && testSrc.length > 2) {
+                    //         var srcWithoutQuotes = testSrc.replace(/'/g, '');
+                    //         scope.attributes.static.value[i].parsedSrc = srcWithoutQuotes;
+                    //     } else {
+                    //         scope.attributes.static.value[i].parsedSrc = scope.attributes.static.value[i].src;
+                    //     }
+                    // }
                 }
                 scope.rebuildCarousel = function() {
                     if ( scope.attributes.optionsType.value === 'static' && scope.attributes.static.value.length > 0 ) {
