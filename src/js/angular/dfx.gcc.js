ac0a59eb500be7ab159357f6ead0921d578cd75f
Andriy <andriy292253@gmail.com>
2016-10-10 23:41:22 +0300

    DREAM: 661

--- src/js/angular/dfx.gcc.js
+++ src/js/angular/dfx.gcc.js
@@ -2066,62 +2066,14 @@ dfxGCC.directive('dfxGccWebKnob', ['$timeout', '$compile', function($timeout, $c
         link: function (scope, element, attrs, basectrl) {
             var component = scope.$parent.getComponent(element);
             basectrl.init(scope, element, component, attrs, 'knob').then(function () {
-                scope.attributes.binding.status = 'overridden';
-                if ( !scope.attributes.options.value.hasOwnProperty('size') ){
-                    scope.attributes.options.value = {
-                        "animate": {"enabled":true,"duration":1000,"ease":"bounce"},
-                        "barCap": 20,
-                        "barColor": "#e65d5d",
-                        "barWidth": 40,
-                        "bgColor": "",
-                        "fontSize": "auto",
-                        "displayInput": true,
-                        "dynamicOptions": true,
-                        "displayPrevious": false,
-                        "size": 300,
-                        "min": 0,
-                        "max": 100,
-                        "step": 1,
-                        "startAngle": 0,
-                        "endAngle": 360,
-                        "textColor": "#222222",
-                        "prevBarColor": "rgba(0,0,0,0)",
-                        "trackColor": "#ffe6e6",
-                        "trackWidth": 50,
-                        "readOnly": false,
-                        "unit": "%",
-                        "subText": {"enabled":true, "text":"Sub text", "color":"#808080", "font":"auto"},
-                        "skin": {"type":"tron","width":10,"color":"rgba(255,0,0,.5)","spaceWidth":5},
-                        "scale": {"enabled":true,"type":"lines","color":"#808080","width":3,"quantity":20,"height":10,"spaceWidth":15}
-                    };
+                scope.isLoaded = {"value": false};
+                if(typeof scope.attributes.options.value.readOnly === 'string'){
+                    switch(scope.attributes.options.value.readOnly){
+                        case 'true': scope.attributes.options.value.readOnly = true; break;
+                        case 'false': scope.attributes.options.value.readOnly = false; break;
+                    }
                 }
-                scope.attributes.options.status = 'overridden';
-                $timeout(function() {
-                    scope.isRepeatable = {"value":false};
-                    if(typeof scope.attributes.options.value.readOnly === 'string'){
-                        switch(scope.attributes.options.value.readOnly){
-                            case 'true': scope.attributes.options.value.readOnly = true; break;
-                            case 'false': scope.attributes.options.value.readOnly = false; break;
-                        }
-                    }
-                    if(scope.attributes.binding.value.indexOf('$dfx_item') >= 0) {
-                        scope.isRepeatable.value = true;
-                        scope.repeated_id = Math.floor(Math.random() * 100000);
-                        var repeatedKnobId = component.id+'_dfx_ng_knob_'+scope.repeated_id;
-                        $timeout(function() {
-                            var repeatedKnob = angular.element(document.getElementById(repeatedKnobId));
-                            repeatedKnob.attr('value', scope.attributes.binding.value);
-                            scope = repeatedKnob.scope();
-                            $injector = repeatedKnob.injector();
-                            $injector.invoke(function($compile){
-                                $compile(repeatedKnob)(scope);
-                            })
-                        }, 0);
-                    } else {
-                        $('.'+component.id+'_dfx_ng_knob').empty().html('<ui-knob value="' + scope.attributes.binding.value + '" options="attributes.options.value"></ui-knob>');
-                        $timeout(function() {$compile($('.'+component.id+'_dfx_ng_knob').contents())(scope);}, 0);
-                    }
-                }, 0);
+                scope.isLoaded.value = true;
             });
         }
     }
